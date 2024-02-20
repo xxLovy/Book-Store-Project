351914acf941a2bf66a4ddbe2f4f0c9a3ffcbec9
xxLovy <1xxlovyxx1@gmail.com>
2024-02-20 23:30:15 +0800

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import ShowBook from './pages/CreateBook'
+import EditBook from './pages/EditBook'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/books/create' element={<CreateBook />}></Route>
+      <Route path='/books/details/:id' element={<ShowBook />}></Route>
+      <Route path='/books/edit/:id' element={<EditBook />}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook />}></Route>
+    </Routes>
+  )
+}
+
+export default App
